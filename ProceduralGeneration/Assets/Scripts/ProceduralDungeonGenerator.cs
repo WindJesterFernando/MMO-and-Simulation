799d5479d5bcc5f5769b24de6d9dd0d4ff2cdefe
Fernando Restituto <linksgreenhat@gmail.com>
2025-01-14 17:33:23 +0100

    pondering some more

--- ProceduralGeneration/Assets/Scripts/ProceduralDungeonGenerator.cs
+++ ProceduralGeneration/Assets/Scripts/ProceduralDungeonGenerator.cs
@@ -10,13 +10,13 @@ public static partial class ProceduralDungeonGenerator
         AddRoom(RoomType.Start, new Coordinate(0, 0));
 
 
-        // AddRoom(RoomType.Normal, new Coordinate(1, 0));
-        // AddRoom(RoomType.Normal, new Coordinate(-1, 0));
+        AddRoom(RoomType.Normal, new Coordinate(1, 0));
+        AddRoom(RoomType.Normal, new Coordinate(-1, 0));
 
-        // if (Roll(50))
-        // {
-        //     AddRoom(RoomType.Normal, new Coordinate(0, -1));
-        // }
+        if (Roll(50))
+        {
+            AddRoom(RoomType.Normal, new Coordinate(0, -1));
+        }
 
 
         // for (int i = 0; i < 100; i++)
@@ -35,21 +35,22 @@ public static partial class ProceduralDungeonGenerator
 
         //
 
+        int newX = (int)(GetRandomValueFromZeroToOne() * 2);
+        int newY = (int)(GetRandomValueFromZeroToOne() * 2);
+
+        Coordinate coord = new Coordinate(newX, newY);
+
+        UnityEngine.Debug.Log(coord.x + "," + coord.y);
+        
+
         foreach (Room r in GetDungeonRooms())
         {
-            int newX = (int)(GetRandomValueFromZeroToOne() * 2);
-            int newY = (int)(GetRandomValueFromZeroToOne() * 2);
-
-            Coordinate coord = new Coordinate(newX, newY);
-
-            UnityEngine.Debug.Log(coord.x + "," + coord.y);
-
             if (r.coordinate.x == coord.x && r.coordinate.y == coord.y)
                 UnityEngine.Debug.Log("exists");
             else
                 UnityEngine.Debug.Log("does not exists");
-                //room exists!!
-                //UnityEngine.Debug.Log(r.coordinate.x + "," + r.coordinate.y);
+            //room exists!!
+            //UnityEngine.Debug.Log(r.coordinate.x + "," + r.coordinate.y);
         }
 
 
